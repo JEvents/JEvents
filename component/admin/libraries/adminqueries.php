4a4ebacf2badf97fff848a4796e35d080d20b3f1
Geraint Edwards <github@copyn.plus.com>
2010-05-20 13:30:26 +0000

    Check of keywords - nothing changed

--- component/admin/libraries/adminqueries.php
+++ component/admin/libraries/adminqueries.php
@@ -7,7 +7,7 @@
  * @copyright   Copyright (C)  2008-2009 GWE Systems Ltd, 2006-2008 JEvents Project Group
  * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
  * @link        http://www.jevents.net
- */
+ */ 
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
