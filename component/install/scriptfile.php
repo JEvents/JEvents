ad69005a4bbcc0f3b9587dc121fbb60fe8f334ee
Geraint Edwards <github@copyn.plus.com>
2010-09-28 12:39:35 +0000

    Improved scriptfile for component installer

--- component/install/scriptfile.php
+++ component/install/scriptfile.php
@@ -1,7 +1,38 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-?>
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class com_jeventsInstallerScript
+{
+
+	public function preflight($action, $adapter)
+	{
+		return true;
+	}
+
+	public function update()
+	{
+		return true;
+	}
+
+	public function install($adapter)
+	{
+		return true;
+	}
+
+	public function postflight($action, $adapter)
+	{
+		?>
+			<script  type="text/javascript" language="javascript">
+				document.location.replace("<?php echo JURI::root();?>administrator/index.php?option=com_jevents");
+			</script>
+		<?php
+	}
+
+	public function uninstall($adapter)
+	{
+		return true;
+	}
+
+}
