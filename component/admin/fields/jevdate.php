94839dbb82983ecd5c8c822562ea57d46a62ebc3
Royce Harding <royce@dtandt.com>
2010-10-06 13:01:08 +0000

    New custom type - currently unused!

---
+++ component/admin/fields/jevdate.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * JEvents Component for Joomla 1.5.x
+ *
+ * @version     $Id$
+ * @package     JEvents
+ * @copyright   Copyright (C) 2008-2009 GWE Systems Ltd
+ * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
+ * @link        http://www.jevents.net
+ */
+
+// Check to ensure this file is included in Joomla!
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+JFormHelper::loadFieldClass('calendar');
+
+class JFormFieldJEVDate extends JFormFieldCalendar
+{
+	public $type = 'JEVDate';  // must be public as per JFormFieldCalendar!! 
+	
+	public function getInput()
+	{
+		echo 'here';
+		parent::getInput();
+	}
+
+}
