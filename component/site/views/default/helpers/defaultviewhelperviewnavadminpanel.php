120f950835841c248284ace96f4eb28130d3404c
Royce Harding <royce@dtandt.com>
2010-10-20 13:37:14 +0000

    Fix parse error, add header comments and $Id$

--- component/site/views/default/helpers/defaultviewhelperviewnavadminpanel.php
+++ component/site/views/default/helpers/defaultviewhelperviewnavadminpanel.php
@@ -1,4 +1,14 @@
 <?php 
+/**
+ * JEvents Component for Joomla 1.5.x
+ *
+ * @version     $Id$
+ * @package     JEvents
+ * @copyright   Copyright (C) 2008-2009 GWE Systems Ltd, 2006-2008 JEvents Project Group
+ * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
+ * @link        http://www.jevents.net
+ */
+ 
 defined('_JEXEC') or die('Restricted access');
 
 function DefaultViewHelperViewNavAdminPanel($view){
@@ -53,7 +63,7 @@ function DefaultViewHelperViewNavAdminPanel($view){
 	                        <?php
                         }
 
-                        if(( $user->id>0) {
+                        if ( $user->id>0) {
                         	$datamodel	=new JEventsDataModel();
                         	// find appropriate Itemid and setup catids for datamodel
                         	$myItemid = JEVHelper::getItemid();
