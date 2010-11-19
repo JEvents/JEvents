0d3e39ee302c52f62d58b6abae5591885d850e4a
Royce Harding <royce@dtandt.com>
2010-11-19 19:43:19 +0000

    Add keyword property to SVn

--- component/admin/libraries/jevlayout_1.6.php
+++ component/admin/libraries/jevlayout_1.6.php
@@ -4,7 +4,7 @@
  *
  * Installer adapter for jevents layouts
  * 
- * @version     $Id:$
+ * @version     $Id$
  * @package     JEvents
  * @copyright   Copyright (C) 2008-2009 GWE Systems Ltd
  * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
