d0c01c3cc29da75c510e1e2cc11e03c299d0dbaf
Geraint Edwards <github@copyn.plus.com>
2011-05-15 08:44:27 +0000

    Fix language strings (missing quotes at start!

--- component/admin/views/defaults/tmpl/edit_icalevent.list_row.php
+++ component/admin/views/defaults/tmpl/edit_icalevent.list_row.php
@@ -40,6 +40,10 @@ defaultsEditorPlugin.node(optgroup , "<?php echo JText::_("JEV_FIELD_CATEGORY",t
 defaultsEditorPlugin.node(optgroup , "<?php echo JText::_("JEV_FIELD_CATEGORY_LINK",true);?>", "CATEGORYLNK");
 defaultsEditorPlugin.node(optgroup , "<?php echo JText::_("JEV_FIELD_COLOUR",true);?>", "COLOUR");
 defaultsEditorPlugin.node(optgroup , "<?php echo JText::_("JEV_FIELD_CREATIONDATE",true);?>", "CREATED");
+defaultsEditorPlugin.node(optgroup , "<?php echo JText::_("JEV_FIELD_LINKSTART",true);?>", "LINKSTART");
+defaultsEditorPlugin.node(optgroup , "<?php echo JText::_("JEV_FIELD_LINKEND",true);?>", "LINKEND");
+defaultsEditorPlugin.node(optgroup , "<?php echo JText::_("JEV_FIELD_URL",true);?>", "URL");
+
 
 <?php
 // get list of enabled plugins
