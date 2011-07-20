7e163753717838597b8909924e6ccf209123ae16
Geraint Edwards <github@copyn.plus.com>
2011-07-20 13:18:19 +0000

    2.0 version RC12 Reset select options in MSIE more reliably

--- modules/mod_jevents_filter/mod_jevents_filter.js
+++ modules/mod_jevents_filter/mod_jevents_filter.js
@@ -7,7 +7,24 @@ var JeventsFilters = {
 			}
 			else {
 				var elem = $(item.id);
-				if (elem) elem.value = item.value;
+				if (elem) {
+					try {
+						var tag = elem.get('tag');
+					}
+					catch (e) {
+						var tag = elem.getTag();
+					}
+					if (tag =='select'){
+						$ES('option',elem).each(
+							function(selitem){
+								selitem.selected=(selitem.value==item.value)?true:false;
+							}
+						);
+					}
+					else {
+						elem.value = item.value;
+					}
+				}
 
 			}
 		});
