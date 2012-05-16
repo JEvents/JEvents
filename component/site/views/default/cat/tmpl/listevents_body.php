2dd0a29ae6347e7dbfd881c9eca81c3cb7a0fa5c
Geraint Edwards <github@copyn.plus.com>
2012-05-16 11:17:34 +0100

    Fix handling of exceptions table when deleting future events end time and start time blanked appropriately in layout editor when allday or noendtime events

--- component/site/views/default/cat/tmpl/listevents_body.php
+++ component/site/views/default/cat/tmpl/listevents_body.php
@@ -18,7 +18,7 @@ if (JRequest::getInt("category_fv",-1)==-1) {
  */
 $cfg	 = & JEVConfig::getInstance();
 $data = $this->datamodel->getCatData( $this->catids,$cfg->get('com_showrepeats',0), $this->limit, $this->limitstart);
-
+$this->data = $data;
 $Itemid = JEVHelper::getItemid();
 
 ?>
