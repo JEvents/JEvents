0f668067f732cea1ac9992b4d5a8d44edc249fc1
Geraint Edwards <github@copyn.plus.com>
2012-08-20 10:28:18 +0000

    v 2.2.5

--- component/admin/libraries/version.php
+++ component/admin/libraries/version.php
@@ -21,7 +21,7 @@ class JEventsVersion {
 	/** @var int Sub Release Level */
 	var $DEV_LEVEL 	= '2';
 	/** @var string Patch Level */
-	var $PATCH_LEVEL = '4';
+	var $PATCH_LEVEL = '5';
 	/** @var string Development Status */
 	var $DEV_STATUS = '';
 	/** @var string Copyright Text */
