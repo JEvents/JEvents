b618a2aaf1a02385350b6d3d43315e11aed952a4
Geraint Edwards <github@copyn.plus.com>
2012-10-12 08:04:39 +0000

    Fix for import of events when multi-category is enabled

--- component/site/libraries/iCalICSFile.php
+++ component/site/libraries/iCalICSFile.php
@@ -278,7 +278,7 @@ RAWTEXT;
 						$categories = $db->loadObjectList('title');
 
 						$params = JComponentHelper::getParams(JEV_COM_COMPONENT);
-						if ($params->get("multicategory",0) && count($evcat)>1){
+						if ($params->get("multicategory",0) ){
 							$vevent->catid = array();
 							foreach ($evcat as $ct){
 								$vevent->catid[] =  $categories[$ct]->id;
