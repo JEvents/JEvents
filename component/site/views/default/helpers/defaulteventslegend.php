131ec461ba6e927c35d511587c1086cc0dd602c5
tonys-xps <tonys-xps@tonys-xps-PC.lan>
2012-10-29 09:46:29 +0000

    Added an if file exists for legend, as when layouts are installed the system looks for this in monthly view.

--- component/site/views/default/helpers/defaulteventslegend.php
+++ component/site/views/default/helpers/defaulteventslegend.php
@@ -2,26 +2,26 @@
 defined('_JEXEC') or die('Restricted access');
 
 if (file_exists(JPATH_SITE."/modules/mod_jevents_legend/helper.php")){
-function DefaultEventsLegend($view){
-	$cfg = & JEVConfig::getInstance();
-	$theme = JEV_CommonFunctions::getJEventsViewName();
+	function DefaultEventsLegend($view){
+		$cfg = & JEVConfig::getInstance();
+		$theme = JEV_CommonFunctions::getJEventsViewName();
 
-	$modpath = JModuleHelper::getLayoutPath('mod_jevents_legend',$theme.'/'."legend");
-	if (!file_exists($modpath)) return;
+		$modpath = JModuleHelper::getLayoutPath('mod_jevents_legend',$theme.'/'."legend");
+		if (!file_exists($modpath)) return;
 
-	// load the helper class
-	require_once (JPATH_SITE.'/modules/mod_jevents_legend/helper.php');
-	require_once($modpath);
+		// load the helper class
+		require_once (JPATH_SITE.'/modules/mod_jevents_legend/helper.php');
+		require_once($modpath);
 
-	$viewclass = ucfirst($theme)."ModLegendView";
-	$module = JModuleHelper::getModule("mod_jevents_legend",false);
+		$viewclass = ucfirst($theme)."ModLegendView";
+		$module = JModuleHelper::getModule("mod_jevents_legend",false);
 
-	$params = new JRegistry( $module->params );
+		$params = new JRegistry( $module->params );
 
-	$modview = new $viewclass($params, $module->id);
-	echo $modview->displayCalendarLegend("block");
+		$modview = new $viewclass($params, $module->id);
+		echo $modview->displayCalendarLegend("block");
 
-	echo "<br style='clear:both;height:0px;line-height:0px;'/>";
-}
+		echo "<br style='clear:both;height:0px;line-height:0px;'/>";
+	}
 }
 
