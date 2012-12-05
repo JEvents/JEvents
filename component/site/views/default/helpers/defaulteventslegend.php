cb448c14ae0ee1bd7ec8b768a41db65a7cc2fe2d
Geraint Edwards <github@copyn.plus.com>
2012-12-05 08:51:47 +0000

    test legend is installed properly - needed because of changes to new layout installation methods

--- component/site/views/default/helpers/defaulteventslegend.php
+++ component/site/views/default/helpers/defaulteventslegend.php
@@ -1,25 +1,30 @@
-<?php 
+<?php
+
 defined('_JEXEC') or die('Restricted access');
 
-function DefaultEventsLegend($view){
+function DefaultEventsLegend($view)
+{
 	$cfg = & JEVConfig::getInstance();
 	$theme = JEV_CommonFunctions::getJEventsViewName();
 
-	$modpath = JModuleHelper::getLayoutPath('mod_jevents_legend',$theme.DS."legend");
-	if (!file_exists($modpath)) return;
+	$modpath = JModuleHelper::getLayoutPath('mod_jevents_legend', $theme . '/' . "legend");
+	if (!file_exists($modpath) || !file_exists(JPATH_SITE . '/modules/mod_jevents_legend/helper.php'))
+		return;
 
-	// load the helper class
-	require_once (JPATH_SITE.'/modules/mod_jevents_legend/helper.php');
+
+// load the helper class
+	require_once (JPATH_SITE . '/modules/mod_jevents_legend/helper.php');
 	require_once($modpath);
 
-	$viewclass = ucfirst($theme)."ModLegendView";
-	$module = JModuleHelper::getModule("mod_jevents_legend",false);
+	$viewclass = ucfirst($theme) . "ModLegendView";
 
-	$params = new JParameter( $module->params );
+	$module = JModuleHelper::getModule("mod_jevents_legend", false);
+	$params = new JRegistry($module->params);
 
 	$modview = new $viewclass($params, $module->id);
 	echo $modview->displayCalendarLegend("block");
 
 	echo "<br style='clear:both;height:0px;line-height:0px;'/>";
+
 }
 
