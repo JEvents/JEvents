cf6065b7f268a19a2f1b815efdf13a81e397c9e5
Tony Partridge <tony@xtech.im>
2013-04-04 19:48:45 +0100

    Updated to work correctly, checks if in event details, then checks if its to show the filter or not. If not in event details it shows all the time.

--- modules/mod_jevents_filter/tmpl/default.php
+++ modules/mod_jevents_filter/tmpl/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * JEvents Component for Joomla 1.5.x
  *
@@ -9,64 +10,77 @@
  * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
  * @link        http://www.gwesystems.com
  */
-
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
-if (JRequest::getCmd("task")=="icalrepeat.detail"){
-	if ($params->get('showindetails', 0) == 0) {
-	   return;
-	}
-} else {
-	
 
-$datamodel	= new JEventsDataModel();
+$datamodel = new JEventsDataModel();
 // find appropriate Itemid and setup catids for datamodel
 $Itemid = JRequest::getInt("Itemid");
 $option = JRequest::getCmd("option");
-if ($option==JEV_COM_COMPONENT){
+if ($option == JEV_COM_COMPONENT)
+{
 	$myItemid = $Itemid;
 }
-else {
-	$myItemid = $params->get("target_itemid",0);
+else
+{
+	$myItemid = $params->get("target_itemid", 0);
 }
-if ($myItemid ==0){
+if ($myItemid == 0)
+{
 	$myItemid = $datamodel->setupModuleCatids($params);
 }
 
 $form_link = "";
-if ($myItemid>0){
+if ($myItemid > 0)
+{
 	$menu = & JSite::getMenu();
 	$menuitem = $menu->getItem($myItemid);
-        if ($menuitem){
-            $form_link = $menuitem->link. "&Itemid=".$myItemid;
-        }
+	if ($menuitem)
+	{
+		$form_link = $menuitem->link . "&Itemid=" . $myItemid;
+	}
 }
 
 //$myItemid = JEVHelper::getItemid();
 $datamodel->setupComponentCatids();
 
-list($year,$month,$day) = JEVHelper::getYMD();
-$evid = JRequest::getVar("evid",false);
-$jevtype = JRequest::getVar("jevtype",false);
+list($year, $month, $day) = JEVHelper::getYMD();
+$evid = JRequest::getVar("evid", false);
+$jevtype = JRequest::getVar("jevtype", false);
 // FORM for filter submission
 $tmpCatids = trim($datamodel->catidsOut);
 
-if ($form_link==""){
-	$form_link = 'index.php?option=' . JEV_COM_COMPONENT . '&task=' . JRequest::getVar("jevtask", "cat.listevents"). "&Itemid=".$myItemid;
+if ($form_link == "")
+{
+	$form_link = 'index.php?option=' . JEV_COM_COMPONENT . '&task=' . JRequest::getVar("jevtask", "cat.listevents") . "&Itemid=" . $myItemid;
 }
 
 // category ID gets picked up by POST results!
 $form_link = JRoute::_($form_link
-. ($evid ? '&evid=' . $evid : '')
-. ($jevtype ? '&jevtype=' . $jevtype : '')
-. ($year ? '&year=' . $year : '')
-. ($month ? '&month=' . $month : '')
-. ($day ? '&day=' . $day : '')
-,false);
+				. ($evid ? '&evid=' . $evid : '')
+				. ($jevtype ? '&jevtype=' . $jevtype : '')
+				. ($year ? '&year=' . $year : '')
+				. ($month ? '&month=' . $month : '')
+				. ($day ? '&day=' . $day : '')
+				, false);
 
 $filters = $jevhelper->getFilters();
 $filterHTML = $filters->getFilterHTML();
 
-require(JModuleHelper::getLayoutPath('mod_jevents_filter', 'default_layout'));
 
+
+if (JRequest::getCmd("task") == "icalrepeat.detail")
+{
+	if ($params->get('showindetails', 0) == 0)
+	{
+		return;
+	}
+	else
+	{
+		require(JModuleHelper::getLayoutPath('mod_jevents_filter', 'default_layout'));
+	}
+}
+else
+{
+	require(JModuleHelper::getLayoutPath('mod_jevents_filter', 'default_layout'));
 }
