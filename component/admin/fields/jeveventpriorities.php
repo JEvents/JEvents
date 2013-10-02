b67550350af9eca8513150614fff9d66a9edcbe5
Owen <Owen@192.168.1.4>
2013-10-02 16:17:02 +0100

    fix for priority field when it isnt shown

--- component/admin/fields/jeveventpriorities.php
+++ component/admin/fields/jeveventpriorities.php
@@ -25,12 +25,12 @@ class JFormFieldJeveventpriorities extends JFormField
 	{	
 		$params = & JComponentHelper::getParams(JEV_COM_COMPONENT);
 		$showpriority = $params->get("showpriority", 0);
-                JLoader::register('JEVHelper',JPATH_SITE."/components/com_jevents/libraries/helper.php");
-                JEVHelper::ConditionalFields( $this->element,$this->form->getName()); 
-		
+               
 		// only those who can publish globally can set priority field
 		if ($showpriority && JEVHelper::isEventPublisher(true))
 		{
+                     JLoader::register('JEVHelper',JPATH_SITE."/components/com_jevents/libraries/helper.php");
+                     JEVHelper::ConditionalFields( $this->element,$this->form->getName()); 		
 			$list = array();
 			for ($i = 0; $i < 10; $i++)
 			{
