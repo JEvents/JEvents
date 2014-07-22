2bb746f2a70f1b50fd4a57fcc62096e029f4b151
Geraint Edwards <github@copyn.plus.com>
2014-07-22 08:28:09 +0000

    improved build script to handle paths etc.

--- build/createpackage.sh
+++ build/createpackage.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 find ./ -type f -name *.zip | xargs rm -f
 VERSION=`xml_grep version ../pkg_jevents.xml --text`
-VERSION="3.2.0_jq2"
+VERSION="3.2.0_jq3"
 rsync -av --progress ../ ./ --exclude build
 zip  -r com_jevents.zip component -x *.svn*
-zip  -r --exclude=*.svn* finder.zip plugins/finder
-zip  -r --exclude=*.svn* search.zip plugins/search
-zip  -r --exclude=*.svn* googl.zip libraries/googl
-zip  -r --exclude=*.svn* mod_jevents_cal.zip modules/mod_jevents_cal
-zip  -r --exclude=*.svn* mod_jevents_custom.zip modules/mod_jevents_custom
-zip  -r --exclude=*.svn* mod_jevents_filter.zip modules/mod_jevents_filter
-zip  -r --exclude=*.svn* mod_jevents_latest.zip modules/mod_jevents_latest
-zip  -r --exclude=*.svn* mod_jevents_legend.zip modules/mod_jevents_legend
-zip  -r --exclude=*.svn* mod_jevents_switchview.zip modules/mod_jevents_switchview
-zip  -r --exclude=*.svn* "Jevents_$VERSION.zip" install.php pkg_jevevents.xml com_jevents.zip finder.zip search.zip googl.zip  mod_jevents_cal.zip mod_jevents_custom.zip mod_jevents_filter.zip   mod_jevents_latest.zipmod_jevents_legend.zip mod_jevents_switchview.zip
+cd plugins
+zip  -r --exclude=*.svn* ../finder.zip finder
+zip  -r --exclude=*.svn* ../search.zip search
+cd ../
+cd libraries
+zip  -r --exclude=*.svn* ../googl.zip googl
+cd ../
+cd modules
+zip  -r --exclude=*.svn* ../mod_jevents_cal.zip mod_jevents_cal
+zip  -r --exclude=*.svn* ../mod_jevents_custom.zip mod_jevents_custom
+zip  -r --exclude=*.svn* ../mod_jevents_filter.zip mod_jevents_filter
+zip  -r --exclude=*.svn* ../mod_jevents_latest.zip mod_jevents_latest
+zip  -r --exclude=*.svn* ../mod_jevents_legend.zip mod_jevents_legend
+zip  -r --exclude=*.svn* ../mod_jevents_switchview.zip mod_jevents_switchview
+cd ../
+zip  -r --exclude=*.svn* "Jevents_$VERSION.zip" install.php pkg_jevents.xml com_jevents.zip finder.zip search.zip googl.zip  mod_jevents_cal.zip mod_jevents_custom.zip mod_jevents_filter.zip   mod_jevents_latest.zip mod_jevents_legend.zip mod_jevents_switchview.zip
 
 find . \! -name "createpackage.sh" \! -name "Jevents_$VERSION.zip" | xargs rm -rf
