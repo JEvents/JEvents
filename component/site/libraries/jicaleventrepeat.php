a13ecb6ce515418ac1288842614cf11bf213aa2d
Geraint Edwards <github@copyn.plus.com>
2014-11-04 11:25:42 +0000

    Fix for chosen weekday checkboxes not being updated when editing reopreating events Fix for ajax search modules making JEvets URL always in a popup

--- component/site/libraries/jicaleventrepeat.php
+++ component/site/libraries/jicaleventrepeat.php
@@ -237,7 +237,7 @@ class jIcalEventRepeat extends jIcalEventDB
 		$title = JFilterOutput::stringURLSafe($this->title());
 		$link = "index.php?option=" . JEV_COM_COMPONENT . "&task=" . $this->detailTask() . "&evid=" . $this->rp_id() . '&Itemid=' . $Itemid
 				. "&year=$year&month=$month&day=$day&title=" . $title . "&uid=" . urlencode($this->uid());
-		if (JRequest::getCmd("tmpl", "") == "component" && JRequest::getCmd('task', 'selectfunction') != 'icalevent.select' && JRequest::getCmd("option", "") != "com_acymailing" && JRequest::getCmd("option", "") != "com_jnews" && JRequest::getCmd("jevtask", "") != "crawler.listevents")
+		if (JRequest::getCmd("tmpl", "") == "component" && JRequest::getCmd('task', 'selectfunction') != 'icalevent.select' && JRequest::getCmd("option", "") != "com_acymailing" && JRequest::getCmd("option", "") != "com_jnews" && JRequest::getCmd("option", "") != "com_search" && JRequest::getCmd("jevtask", "") != "crawler.listevents")
 		{
 			$link .= "&tmpl=component";
 		}
