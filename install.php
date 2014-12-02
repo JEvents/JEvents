64d1a2f8a5abb8c97bd7885be933b2fbcff45dc1
Geraint Edwards <github@copyn.plus.com>
2014-12-02 15:15:37 +0000

    JEvents 3.1.34 release

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
  * JEvents Component for Joomla 2.5.x
  *
  * @version     3.1.34
- * @releasedate November 2014
+ * @releasedate December 2014
  * @package     JEvents
  * @copyright   Copyright (C) 2008-2012 GWE Systems Ltd, 2006-2008 JEvents Project Group
  * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
