66d4f6b3ee6d0c92a7cba501bdfe871d4c340d4b
Geraint Edwards <github@copyn.plus.com>
2015-01-27 15:07:57 +0000

    enable bootstrap chosen config option in Joomla 2.5 too dummy commit to force frontend models folder to be there

--- component/site/models/icalevent.php
+++ component/site/models/icalevent.php
@@ -2,5 +2,5 @@
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die( 'Restricted access' );
-
+// Load the backend model file
 include(JEV_ADMINPATH."/models/".basename(__FILE__));
