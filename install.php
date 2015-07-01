33a84aff6b57eecd297dca3931bf52514b6a0466
Geraint Edwards <github@copyn.plus.com>
2015-07-01 09:26:33 +0100

    July 2015  !!!

--- install.php
+++ install.php
@@ -3,7 +3,7 @@
 /**
  * JEvents Component for Joomla 2.5.x
  *
- * @version     3.2.16
+ * @version     3.2.18
  * @releasedate January 2015
  * @package     JEvents
  * @copyright   Copyright (C) 2008-2015 GWE Systems Ltd, 2006-2008 JEvents Project Group
