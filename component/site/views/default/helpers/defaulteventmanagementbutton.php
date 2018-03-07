4f9b2f87b18fa54d35d6f318c2e94df69610c9cc
Tony Partridge <admin@xtech.im>
2018-03-07 09:28:41 +0000

    updated to use JEvents modal script

--- component/site/views/default/helpers/defaulteventmanagementbutton.php
+++ component/site/views/default/helpers/defaulteventmanagementbutton.php
@@ -5,7 +5,7 @@ function DefaultEventManagementButton($view, $row)
 {
    	JEVHelper::script( 'view_detailJQ.js', 'components/'.JEV_COM_COMPONENT."/assets/js/" );
 	?>
-	<a href="#my-modal" data-toggle="modal"  data-target="#action_dialogJQ<?php echo $row->rp_id();?>"  title="<?php echo JText::_('JEV_E_EDIT', true); ?>">
+	<a href="javascript:jevEditOptPopup('action_dialogJQ<?php echo $row->rp_id();?>');" title="<?php echo JText::_('JEV_E_EDIT', true); ?>">
 		<?php echo JHtml::image('com_jevents/icons-32/edit.png',JText::_("JEV_E_EDIT"),null,true); ?>
 	</a>
 	<?php
