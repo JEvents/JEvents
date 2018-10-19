30d7db1d90c7a450b3c14d74b8f102659f94e5fa
Tony Partridge <admin@xtech.im>
2018-10-19 10:28:01 +0100

    Add in next repeat to timesheet where applicable, also show the timesheet to be the events actual time sheet.

--- component/admin/views/icalevent/tmpl/overview.php
+++ component/admin/views/icalevent/tmpl/overview.php
@@ -170,9 +170,14 @@ $mainspan = 10;
 							}
 							else
 							{
+
+								$firstRepeat = $row->getFirstRepeat();
 								$times = '<table style="border: 1px solid #666666; width:100%;">';
-								$times .= '<tr><td>' . JText::_('JEV_FROM') . ' : ' . ($row->alldayevent() ? JString::substr($row->publish_up(), 0, 10) : JString::substr($row->publish_up(),0,16)) . '</td></tr>';
+								$times .= '<tr><td>' . JText::_('JEV_FROM') . ' : ' . ($firstRepeat->alldayevent() ? JString::substr($firstRepeat->publish_up(), 0, 10) : JString::substr($firstRepeat->publish_up(),0,16)) . '</td></tr>';
 								$times .= '<tr><td>' . JText::_('JEV_TO') . ' : ' . (($row->noendtime() || $row->alldayevent()) ? JString::substr($row->publish_down(), 0, 10) : JString::substr($row->publish_down(),0,16)) . '</td></tr>';
+								if ($row->hasrepetition()) {
+									$times .= '<tr><td>' . JText::_('Next Repeat:') . ' : ' . ($row->alldayevent() ? JString::substr($row->publish_up(), 0, 10) : JString::substr($row->publish_up(),0,16)) . '</td></tr>';
+								}
 								$times .="</table>";
 								echo $times;
 							}
