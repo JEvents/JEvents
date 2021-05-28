e4b00f19e891c142b28989d643f979614b8c5673
Geraint Edwards <jevents@gwesystems.com>
2021-05-28 16:52:15 +0100

    v3.6.15 release

--- changelogs/3.6.15.php
+++ changelogs/3.6.15.php
@@ -1,9 +1,8 @@
 <?php
-$changelog["package_jevents"]["3.6.14"] = array();
-$changelog["package_jevents"]["3.6.14"]["date"] = "2020-05-26";
-$changelog["package_jevents"]["3.6.14"]["features"] = array();
-$changelog["package_jevents"]["3.6.14"]["bugfixes"] = array();
-$changelog["package_jevents"]["3.6.14"]["bugfixes"][]="Do not apply gsl-select styling to hidden select elements such as persons popup selector";
-
+$changelog["package_jevents"]["3.6.15"] = array();
+$changelog["package_jevents"]["3.6.15"]["date"] = "2020-05-28";
+$changelog["package_jevents"]["3.6.15"]["features"] = array();
+$changelog["package_jevents"]["3.6.15"]["bugfixes"] = array();
+$changelog["package_jevents"]["3.6.15"]["bugfixes"][]="Make sure popover tooltips don't load bootstrap css if jevents config is NOT to load bootstrap";
 
 
