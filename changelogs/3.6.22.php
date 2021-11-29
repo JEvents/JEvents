c002a2e2507fe7aefdcad9da53f276317a7b897f
Geraint Edwards <jevents@gwesystems.com>
2021-11-29 12:34:20 +0000

    update changelog file

---
+++ changelogs/3.6.22.php
@@ -0,0 +1,10 @@
+<?php
+$changelog["package_jevents"]["3.6.22"] = array();
+$changelog["package_jevents"]["3.6.22"]["date"] = "2020-11-29";
+$changelog["package_jevents"]["3.6.22"]["features"] = array();
+$changelog["package_jevents"]["3.6.22"]["bugfixes"] = array();
+
+$changelog["package_jevents"]["3.6.22"]["bugfixes"][]="Fix translations modals in J4";
+$changelog["package_jevents"]["3.6.22"]["bugfixes"][]="Translations now use UIkit styling in backend and when enabled in the frontend";
+$changelog["package_jevents"]["3.6.22"]["bugfixes"][]="Fix for bootstrap 5 tooltips";
+$changelog["package_jevents"]["3.6.22"]["bugfixes"][]="Fix for Joomla 4 caching - there may still be javascript bugs due to bug in Joomla 4 - see https://github.com/joomla/joomla-cms/pull/36068";
