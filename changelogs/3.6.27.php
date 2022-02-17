38b4b32dcb8f41bf651849ff65558d7f9befbd25
Geraint Edwards <jevents@gwesystems.com>
2022-02-17 09:25:41 +0000

    change logs

---
+++ changelogs/3.6.27.php
@@ -0,0 +1,11 @@
+<?php
+$version = "3.6.27";
+$date = "2022-02-17";
+$changelog["package_jevents"][$version] = array();
+$changelog["package_jevents"][$version]["date"] = $date;
+$changelog["package_jevents"][$version]["features"] = array();
+$changelog["package_jevents"][$version]["features"][] = "";
+
+$changelog["package_jevents"][$version]["bugfixes"] = array();
+$changelog["package_jevents"][$version]["bugfixes"][]="Workaround for YooTheme Pro worker streams for editing events in the frontend";
+$changelog["package_jevents"][$version]["bugfixes"][]="Clear plugin caches when saving parameters - to ensure changes in plugin settings take immediate effect";
