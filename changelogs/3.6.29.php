92c26e6702834f3a467e6fb42f7c8e70633f8525
Geraint Edwards <jevents@gwesystems.com>
2022-02-18 14:18:14 +0000

    v.3.6.28 fix for another bloody stupid problem from YooTheme Pro

---
+++ changelogs/3.6.29.php
@@ -0,0 +1,9 @@
+<?php
+$version = "3.6.29";
+$date = "2022-02-18";
+$changelog["package_jevents"][$version] = array();
+$changelog["package_jevents"][$version]["date"] = $date;
+$changelog["package_jevents"][$version]["features"] = array();
+
+$changelog["package_jevents"][$version]["bugfixes"] = array();
+$changelog["package_jevents"][$version]["bugfixes"][]="Workaround for another irritating YooTheme Pro worker streams problem introduced in version 2.7.19";
