4aedb12406fd1765f6e4c73cdb9b9d8972f08264
Geraint Edwards <jevents@gwesystems.com>
2022-04-05 14:41:49 +0100

    v3.6.31 changelog and version

---
+++ changelogs/3.6.31.php
@@ -0,0 +1,14 @@
+<?php
+$version = "3.6.31";
+$date = "2022-04-05";
+$changelog["package_jevents"][$version] = array();
+$changelog["package_jevents"][$version]["date"] = $date;
+$changelog["package_jevents"][$version]["features"] = array();
+$changelog["package_jevents"][$version]["features"][] = "Add optional framework selection in JEvents to allow usage of appropriate modals and popovers - especially important in uikit templates to set this";
+
+$changelog["package_jevents"][$version]["bugfixes"] = array();
+$changelog["package_jevents"][$version]["bugfixes"][]="Joomla 4 placeholder fix for event title";
+$changelog["package_jevents"][$version]["bugfixes"][]="Include min JS files in package to make sure any stray ones are overwritten";
+$changelog["package_jevents"][$version]["bugfixes"][]="Fix for conditional custom fields during step by step event creation";
+$changelog["package_jevents"][$version]["bugfixes"][]="Switch parameter editing to use native Joomla showon";
+$changelog["package_jevents"][$version]["bugfixes"][]="iCal import modal z-index fix for uikit sites";
