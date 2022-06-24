4280617558e464f5005d00a685724e59d7bd0aac
Geraint Edwards <jevents@gwesystems.com>
2022-06-24 11:54:15 +0100

    v3.6.39

--- changelogs/3.6.39.php
+++ changelogs/3.6.39.php
@@ -8,15 +8,10 @@ $changelog[$extension][$version]["features"]        = array();
 $changelog[$extension][$version]["bugfixes"]        = array();
 
 
-$changelog[$extension][$version]["features"][]      = "New config option to ALWAYS show left menu icons";
-$changelog[$extension][$version]["features"][]      = "New JEvents filter option to use UIKit styling for form elements";
-$changelog[$extension][$version]["features"][]      = "Add custom css option for filter module";
-
-
-$changelog[$extension][$version]["bugfixes"][]      = "Correct showon for tabs in event editing in Joomla 4";
-$changelog[$extension][$version]["bugfixes"][]      = "Fix showon for plugin parameters in JEvents config";
-$changelog[$extension][$version]["bugfixes"][]      = "Support background - foreground contrast colours using 3 character hex colours";
-$changelog[$extension][$version]["bugfixes"][]      = "Custom Event editing page was not picking up custom css or js";
-$changelog[$extension][$version]["bugfixes"][]      = "Fix missing ob_start() in system messages layout";
-$changelog[$extension][$version]["bugfixes"][]      = "Migrate module calendar navigation to native javascript";
-$changelog[$extension][$version]["bugfixes"][]      = "";
+$changelog[$extension][$version]["bugfixes"][]      = "Extend missing event finder handling to post save event redirection - if you changed the category of an event in frontend editing it may not be visible on the current menu item without this.";
+$changelog[$extension][$version]["bugfixes"][]      = "New categories should not have category admin set by default";
+$changelog[$extension][$version]["bugfixes"][]      = "Latest events module to take account of multi-day events starting before 'today' when searching the database when using display on first day only mode";
+$changelog[$extension][$version]["bugfixes"][]      = "Catch repeat creation error where RRULE field had extra stray comma at the end e.g. +1FR,";
+$changelog[$extension][$version]["bugfixes"][]      = "Make calendar label 200 chars by default instead of just 30";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix for modal event search when creating single event menu item";
+$changelog[$extension][$version]["bugfixes"][]      = "Reduce use of javascript 'let' in frontend which caused problems for some old iPads";
