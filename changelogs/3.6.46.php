69f095f230250fa0e4cb333fb83ddebfc60a8633
Geraint Edwards <jevents@gwesystems.com>
2022-12-20 11:22:09 +0000

    v3.6.46 fix for clicking cancel when editing event from manage events menu item in the frontend - no longer shows 'nearest repeat' message Fix for missing LD+JSON meta data when using from customised layouts

--- changelogs/3.6.46.php
+++ changelogs/3.6.46.php
@@ -10,8 +10,6 @@ $changelog[$extension][$version]["bugfixes"]        = array();
 
 $changelog[$extension][$version]["bugfixes"][]      = "Fix for iCal exports in Joomla 3";
 $changelog[$extension][$version]["bugfixes"][]      = "Fix typeahead options e.g. for location search having transparent backgrouns in some templates";
-
-
-
-
-
+$changelog[$extension][$version]["bugfixes"][]      = "Fix for clicking cancel when editing event from manage events menu item in the frontend - no longer shows 'nearest repeat' message";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix for missing LD+JSON meta data when using from customised layouts";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix for 'incorrect prefix key' on installation";
