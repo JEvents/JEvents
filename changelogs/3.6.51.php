a3c286c80bce010c66f6eefb949fabb1447de317
Geraint Edwards <jevents@gwesystems.com>
2023-04-14 16:22:06 +0100

    v3.6.51 Fix stray \ being output by JEvents component

---
+++ changelogs/3.6.51.php
@@ -0,0 +1,11 @@
+<?php
+$version                                            = "3.6.51";
+$date                                               = "2023-04-14";
+$extension                                          = "package_jevents";
+$changelog[$extension][$version]                    = array();
+$changelog[$extension][$version]["date"]            = $date;
+$changelog[$extension][$version]["features"]        = array();
+$changelog[$extension][$version]["bugfixes"]        = array();
+
+
+$changelog[$extension][$version]["bugfixes"][]      = "Fix stray \ being output by JEvents component";
