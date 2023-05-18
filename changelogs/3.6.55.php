fe74eaed0ea50ad0d1fa5924744cade26aaebabb
Geraint Edwards <jevents@gwesystems.com>
2023-05-18 17:29:11 +0100

    v3.6.55

---
+++ changelogs/3.6.55.php
@@ -0,0 +1,10 @@
+<?php
+$version                                            = "3.6.55";
+$date                                               = "2023-05-05";
+$extension                                          = "package_jevents";
+$changelog[$extension][$version]                    = array();
+$changelog[$extension][$version]["date"]            = $date;
+$changelog[$extension][$version]["features"]        = array();
+$changelog[$extension][$version]["bugfixes"]        = array();
+
+$changelog[$extension][$version]["bugfixes"][]      = "Correct strftime function problem in version 3.6.54";
