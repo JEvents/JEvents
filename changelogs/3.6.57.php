8045a81815068b2cd23d6bf3c15dad24492cbfcc
Geraint Edwards <jevents@gwesystems.com>
2023-06-12 15:02:28 +0100

    Update UIkit and build process Add optional use of icalimportkey for reloading anonymously Fix category filtering by level and published state Fix php notice in outlook export urls Support for %b, %B and %h format in calendar fields f

--- changelogs/3.6.57.php
+++ changelogs/3.6.57.php
@@ -7,5 +7,19 @@ $changelog[$extension][$version]["date"]            = $date;
 $changelog[$extension][$version]["features"]        = array();
 $changelog[$extension][$version]["bugfixes"]        = array();
 
+$changelog[$extension][$version]["features"][]      = "Config option to use custom css file in backend event editing ";
 $changelog[$extension][$version]["features"][]      = "Make iCal export URLs nofollow";
+$changelog[$extension][$version]["features"][]      = "UIkit version update - needed for YooTheme sites when editing events";
+$changelog[$extension][$version]["features"][]      = "Add optional use of icalimportkey for reloading anonymously";
+$changelog[$extension][$version]["features"][]      = "Support for %b, %B and %h format in calendar fields";
+
 $changelog[$extension][$version]["bugfixes"][]      = "Move more of strftime usage to date function";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix backend category list view filtering by level and published state";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix php notice in outlook export urls";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix php notice in outlook export urls";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix positioning of close icon in popup modals for BS5";
+
+
+
+
+
