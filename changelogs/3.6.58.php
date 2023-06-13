7adc4e12398ebb03456d0c01fe608e1bf221a971
Geraint Edwards <jevents@gwesystems.com>
2023-06-13 15:46:41 +0100

    v3.6.58 Repackaging of 3.6.57 with correct version of custom UIkit

---
+++ changelogs/3.6.58.php
@@ -0,0 +1,11 @@
+<?php
+$version                                            = "3.6.58";
+$date                                               = "2023-06-13";
+$extension                                          = "package_jevents";
+$changelog[$extension][$version]                    = array();
+$changelog[$extension][$version]["date"]            = $date;
+$changelog[$extension][$version]["features"]        = array();
+$changelog[$extension][$version]["bugfixes"]        = array();
+
+
+$changelog[$extension][$version]["bugfixes"][]      = "Repackaging of 3.6.57 with correct version of custom UIkit";
