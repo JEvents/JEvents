873222fdda4f4c146f4f8a8a4c84c7573522bffb
Geraint Edwards <jevents@gwesystems.com>
2023-07-17 13:15:31 +0100

    v. 3.6.60 Change wording of filter button in list views from 'Search' to 'Filter option' to mirror Joomla 4 changes CSV Printing fix for some servers where the download size didn't match the size quoted

--- changelogs/3.6.60.php
+++ changelogs/3.6.60.php
@@ -10,3 +10,6 @@ $changelog[$extension][$version]["bugfixes"]        = array();
 
 $changelog[$extension][$version]["features"][]      = "New inline printing mechanism that doesn't use popup window - config driven";
 $changelog[$extension][$version]["bugfixes"][]      = "Minor tooltip improvements for Bootstrap 5 in Joomla 4";
+$changelog[$extension][$version]["bugfixes"][]      = "Change wording of filter button in list views from 'Search' to 'Filter option' to mirror Joomla 4 changes";
+$changelog[$extension][$version]["bugfixes"][]      = "CSV Printing fix for some servers where the download size didn't match the size quoted";
+$changelog[$extension][$version]["bugfixes"][]      = "A couple more strftime => date migration fixes";
