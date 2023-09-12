e1515d6503e3c09c59800803060f772c79d61f6b
Geraint Edwards <jevents@gwesystems.com>
2023-09-12 17:13:56 +0100

    v3.6.64 Avoid php notices for passing null values to core methods like trim() Fix for missing onchange handler for calendar fields (caused by recent changes in Joomla popup calendar) New direct strftime to date output function

--- changelogs/3.6.64.php
+++ changelogs/3.6.64.php
@@ -1,5 +1,5 @@
 <?php
-$version                                            = "3.6.65";
+$version                                            = "3.6.64";
 $date                                               = "2023-09-12";
 $extension                                          = "package_jevents";
 $changelog[$extension][$version]                    = array();
@@ -7,4 +7,6 @@ $changelog[$extension][$version]["date"]            = $date;
 $changelog[$extension][$version]["features"]        = array();
 $changelog[$extension][$version]["bugfixes"]        = array();
 
-$changelog[$extension][$version]["bugfixes"][]      = "Fix event date editing for sites using D/M/Y date formats for dates beyond 12th of the month.";
+$changelog[$extension][$version]["bugfixes"][]      = "Avoid php notices for passing null values to core methods like trim()";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix for missing onchange handler for calendar fields (caused by recent changes in Joomla popup calendar)";
+$changelog[$extension][$version]["bugfixes"][]      = "New direct strftime to date output function";
