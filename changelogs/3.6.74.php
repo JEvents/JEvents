021ce18850a0c50b1f85278eced0645a49f55229
Geraint Edwards <jevents@gwesystems.com>
2023-11-17 17:14:54 +0000

    v3.6.74 Fix of installation issue with missing table in version 3.6.73

---
+++ changelogs/3.6.74.php
@@ -0,0 +1,17 @@
+<?php
+$version                                            = "3.6.74";
+$date                                               = "2023-11-17";
+$extension                                          = "package_jevents";
+$changelog[$extension][$version]                    = array();
+$changelog[$extension][$version]["date"]            = $date;
+$changelog[$extension][$version]["features"]        = array();
+$changelog[$extension][$version]["bugfixes"]        = array();
+
+$changelog[$extension][$version]["bugfixes"][]      = "Fix of installation issue with missing table in version 3.6.73";
+
+
+
+
+
+
+
