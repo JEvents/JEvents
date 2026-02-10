15e3b67fcb93da349e327e40e798346df08e6007
Geraint Edwards <jevents@gwesystems.com>
2026-02-10 14:58:51 +0000

    V 3.6.98 Make sure jumpto icon is an option in menu item configs

--- changelogs/3.6.98.php
+++ changelogs/3.6.98.php
@@ -5,7 +5,11 @@ $extension                                          = "package_jevents";
 $changelog[$extension][$version]                    = array();
 $changelog[$extension][$version]["date"]            = $date;
 $changelog[$extension][$version]["features"]        = array();
-//$changelog[$extension][$version]["features"][]      = "";
+$changelog[$extension][$version]["features"][]      = "Allow JumpTo Month icon in all JEvents menu item configurations";
 
 $changelog[$extension][$version]["bugfixes"]        = array();
-$changelog[$extension][$version]["bugfixes"][]      = "";
+$changelog[$extension][$version]["bugfixes"][]      = "Fix for some HTML being stripped from event descriptions and extra info fields in Joomla 6";
+$changelog[$extension][$version]["bugfixes"][]      = "Remove usage of castAsChar";
+$changelog[$extension][$version]["bugfixes"][]      = "fix for irregular date handling when clocks change in Spring";
+
+
