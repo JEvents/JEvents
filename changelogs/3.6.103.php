94a74960e0110c2de2186447e80bc544523fda6f
Geraint Edwards <jevents@gwesystems.com>
2026-06-03 16:13:04 +0100

    v 3.6.103

---
+++ changelogs/3.6.103.php
@@ -0,0 +1,13 @@
+<?php
+$version                                            = "3.6.103";
+$date                                               = "2026-06-03";
+$extension                                          = "package_jevents";
+$changelog[$extension][$version]                    = array();
+$changelog[$extension][$version]["date"]            = $date;
+$changelog[$extension][$version]["features"]        = array();
+$changelog[$extension][$version]["features"][]      = "gslframework respects template overrides";
+
+$changelog[$extension][$version]["bugfixes"]        = array();
+$changelog[$extension][$version]["bugfixes"][]      = "change how we store jevdata in form";
+$changelog[$extension][$version]["bugfixes"][]      = "minor security enhanced for iCalImport";
+
